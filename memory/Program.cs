09ea5932aacdcf3802d0485679ec60bc0290e727
shivamnishad0 <2k23.mca2313672@gmail.com>
2025-09-15 16:16:19 +0530

    Initial Commit

---
+++ memory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Memory
+{
+    static int y = 100; //Global
+    int a = 10;  //instance variable so if we have to use this. First we have to create the object of that the call it...
+    static void Main(string[] args)
+    {
+        int y = 20; // Beacouse this in under the static function....
+        y = 30;
+        Console.WriteLine("Static variable is: " + y);
+        Console.WriteLine("Static Global variable is: " + Memory.y);
+        Memory obj = new Memory();
+        Console.WriteLine("Instance variable is: " + obj.a);
+        const string name = "Shivam Nishad"; // Constant variable mean this value can't be changed 
+        Console.WriteLine(name);
+        Swap.Swaping();
+    }
+}
---
+++ memory/SwapProgram.cs
@@ -0,0 +1,14 @@
+using System;
+
+internal class Swap
+{
+    static int a = 10, b = 20;
+    
+    internal static void Swaping()
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+        Console.WriteLine("value of a={0} and b={1}", a, b);
+    }
+}
