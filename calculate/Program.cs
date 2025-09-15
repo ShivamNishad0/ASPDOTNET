7109e03f888252360c89af6099294c7f18780a8f
shivamnishad0 <2k23.mca2313672@gmail.com>
2025-09-15 14:31:55 +0530

    initial commit

---
+++ calculate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Calculate
+{
+    int a, b, c;
+    void Accept()
+    {
+        Console.WriteLine("Enter the Number to Calculate");
+        Console.WriteLine("Enter the value of A");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the value of B");
+        b = Convert.ToInt32(Console.ReadLine());
+    }
+    void Add() {
+        Console.WriteLine("Addition of the A and B is:- ");
+        c = a + b;
+        Console.WriteLine(c);
+    }
+    void Sub() {
+        Console.WriteLine("Substract of the A and B is:- ");
+        c = a - b;
+        Console.WriteLine(c);
+    }
+    void Mul() {
+        Console.WriteLine("Multiply of the A and B is:- ");
+        c = a * b;
+        Console.WriteLine(c);
+    }
+    void Div() {
+        Console.WriteLine("Division of the A and B is:- ");
+        c = a / b;
+        Console.WriteLine(c);
+    }
+    public static void Main(string[] args)
+    {
+        Calculate obj = new Calculate();
+        obj.Accept();
+        obj.Add();
+        obj.Sub();
+        obj.Mul();
+        obj.Div();
+
+    }
+}
---
+++ shivam/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+public class Great
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, Shivam! Welcome to your new learing...................");
+    }
+}
